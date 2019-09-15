61f0b6f5642c1582b2b7f96699fd7a5826e4d883
Mark Melczer <melczer7@gmail.com>
2019-09-15 18:59:44 +0000

    Line update tested

--- src/line.rs
+++ src/line.rs
@@ -2,6 +2,7 @@
 pub struct Line<'a> {
 	random_line : &'a String,
 	displayed_line : String,
+	random_line_length : usize,
 }
 
 impl<'a> Line<'a> {
@@ -9,6 +10,7 @@ impl<'a> Line<'a> {
 		let mut line = Line {
 			random_line : line,
 			displayed_line : line.clone(),
+			random_line_length : line.len(),
 		};
 		line.mask_display(mask);
 
@@ -23,14 +25,36 @@ impl<'a> Line<'a> {
 			.collect()
 	}
 
+	pub fn update_displayed_line(&mut self, input : &char) -> bool {
+		let lowercase_input = input.to_lowercase().to_string();
+		let uppercase_input = lowercase_input.to_uppercase();
+		if self.random_line.contains(&lowercase_input) || self.random_line.contains(&uppercase_input) {
+			self.displayed_line = self.displayed_line
+				.char_indices()
+				.map(|(i, x)| 
+					 if self.random_line.get(i..i + 1) == Some(&lowercase_input) && x == '_' { 
+					 	 lowercase_input.clone() 
+					 } else if self.random_line.get(i..i + 1) == Some(&uppercase_input) && x == '_' {
+					 	 uppercase_input.clone()
+					 } else { 
+					 	 x.to_string() 
+					 }
+				)
+				.collect();
+			true
+		} else {
+			false
+		}
+	}
+
 	pub fn get_len(&self) -> usize {
-		self.random_line.len()
+		self.random_line_length
 	}
 
 	pub fn get_line(&self) -> &String {
 		self.random_line
 	}
-
+	
 	pub fn get_displayed_line(&mut self) -> &mut String {
 		&mut self.displayed_line
 	}
@@ -85,8 +109,52 @@ mod tests {
 
 		println!("{}", line.get_displayed_line());
 	}
+
+	#[test]
+	fn update_struct() {
+		let state = String::from("AlaBAMa");
+		let mask = vec![0,1,2,3,4,5,6]; 
+
+		let mut line_1 = Line::init(&state, &mask);
+		let mut line_2 = Line::init(&state, &mask);
+
+		assert_eq!(line_1.get_displayed_line(), "_______");
+		assert_eq!(line_2.get_displayed_line(), "_______");
+
+
+		let mut input = 'A';
+		assert!(line_1.update_displayed_line(&mut input));
+
+		input = 'a';
+		assert!(line_2.update_displayed_line(&mut input));
+
+		assert_eq!(line_1.get_displayed_line(), "A_a_A_a");
+		assert_eq!(line_2.get_displayed_line(), "A_a_A_a");
+
+		input = 'k';
+
+		assert!(!line_1.update_displayed_line(&mut input));
+		assert!(!line_2.update_displayed_line(&mut input));
+
+		input = 'b';
+		assert!(line_1.update_displayed_line(&mut input));
+		input = 'B';
+		assert!(line_2.update_displayed_line(&mut input));
+		assert_eq!(line_1.get_displayed_line(), "A_aBA_a");
+		assert_eq!(line_2.get_displayed_line(), "A_aBA_a");
+
+		input = 'm';
+		assert!(line_1.update_displayed_line(&mut input));
+		input = 'M';
+		assert!(line_2.update_displayed_line(&mut input));
+		assert_eq!(line_1.get_displayed_line(), "A_aBAMa");
+		assert_eq!(line_2.get_displayed_line(), "A_aBAMa");
+
+		input = 'L';
+		assert!(line_1.update_displayed_line(&mut input));
+		input = 'l';
+		assert!(line_2.update_displayed_line(&mut input));
+		assert_eq!(line_1.get_displayed_line(), "AlaBAMa");
+		assert_eq!(line_2.get_displayed_line(), "AlaBAMa");
+	}
 }
-
-
-
-
